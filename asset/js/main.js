fba6c9a4d1ad1c8d3464e7d766276b53521bc433
MohammadRadja <you@example.com>
2024-04-05 14:25:24 +0700

    revisi

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,24 +1,42 @@
+// Inisialisasi Carousel
 $("#myCarousel").carousel({
-  interval: 5000,
+  interval: 1000,
 });
 
+// Menambahkan Elemen untuk Next Carousel
 $("#myCarousel .carousel-item").each(function () {
   var minPerSlide = 4;
   var next = $(this).next();
   if (!next.length) {
     next = $(this).siblings(":first");
   }
-  next.children(":first-child").clone().appendTo($(this));
 
   for (var i = 0; i < minPerSlide; i++) {
+    next.children(":first-child").clone().appendTo($(this));
     next = next.next();
     if (!next.length) {
       next = $(this).siblings(":first");
     }
-
-    next.children(":first-child").clone().appendTo($(this));
   }
 });
+
+// Menambahkan Elemen untuk Prev Carousel
+$("#myCarousel .carousel-item").each(function () {
+  var minPerSlide = 4;
+  var prev = $(this).prev();
+  if (!prev.length) {
+    prev = $(this).siblings(":last");
+  }
+
+  for (var i = 0; i < minPerSlide; i++) {
+    prev.children(":last-child").clone().prependTo($(this));
+    prev = prev.prev();
+    if (!prev.length) {
+      prev = $(this).siblings(":last");
+    }
+  }
+});
+
 var containerEl = document.querySelector('[data-ref~="mixitup-container"]');
 
 var mixer = mixitup(containerEl, {
