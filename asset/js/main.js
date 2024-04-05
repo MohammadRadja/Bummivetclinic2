544915a5b9f33d155fd5643a9d4f8d7e8fc9f892
MohammadRadja <you@example.com>
2024-04-05 15:33:46 +0700

    revisi

--- asset/js/main.js
+++ asset/js/main.js
@@ -20,23 +20,6 @@ $("#myCarousel .carousel-item").each(function () {
   }
 });
 
-// Menambahkan Elemen untuk Prev Carousel
-$("#myCarousel .carousel-item").each(function () {
-  var minPerSlide = 4;
-  var prev = $(this).prev();
-  if (!prev.length) {
-    prev = $(this).siblings(":last");
-  }
-
-  for (var i = 0; i < minPerSlide; i++) {
-    prev.children(":last-child").clone().prependTo($(this));
-    prev = prev.prev();
-    if (!prev.length) {
-      prev = $(this).siblings(":last");
-    }
-  }
-});
-
 var containerEl = document.querySelector('[data-ref~="mixitup-container"]');
 
 var mixer = mixitup(containerEl, {
